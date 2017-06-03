e00d6c6adfb92dbc575eeb756c9ac94b9ea8b7bc
Superuser <superuser@jalurkerja.com>
2017-06-03 16:40:20 +0700

    db altered

--- alter_db/alter_db.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE `spk` ADD `work_days_type` SMALLINT NOT NULL AFTER `work_days`;
-ALTER TABLE `spk`
-  DROP `work_start`,
-  DROP `work_end`,
-  DROP `work_days`,
-  DROP `work_days_type`;
